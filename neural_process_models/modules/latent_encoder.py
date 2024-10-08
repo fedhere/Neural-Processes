107be70e397614c0d183673adb5a901c9356baf1
federica <fedhere@gmail.com>
2024-10-07 23:07:10 -0400

    Update latent_encoder.py

--- neural_process_models/modules/latent_encoder.py
+++ neural_process_models/modules/latent_encoder.py
@@ -35,7 +35,8 @@ class LatentEncoder(nn.Module):
         self_attention_type="dot",
         use_self_attn=True,
         attention_layers=2,
-        use_lstm=False #True
+        use_lstm=False,
+	use_lstm=True
     ):
         super().__init__()
         self.input_dim = input_x_dim + input_y_dim
